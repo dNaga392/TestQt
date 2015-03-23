1deede6ae560e4a20a983cba65e02130c213cd73
nagayama <nagayama@ipl.co.jp>
2015-03-23 13:11:04 +0900

    add TestQDir

---
+++ QtCore/TestQDir/testqdir.cpp
@@ -0,0 +1,112 @@
+// QDirのテスト
+// testqdir.cpp
+
+#include <QtTest/QtTest>
+#include <QtCore/QDir>
+
+class TestQDir: public QObject
+{
+	Q_OBJECT
+private slots:
+	void testmkpath();
+	void testmkpath_data();
+	void testcleanabsolutefilepath();
+	void testcleanabsolutefilepath_data();
+	void testpath();
+	void testpath_data();
+};
+
+/// QDir::mkpath() のテスト
+void TestQDir::testmkpath()/*{{{*/
+{
+	// タイトルからテストデータの取得
+	QFETCH(QString, path);
+	QFETCH(bool, bRetVal);
+
+	// 指定のパスのフォルダを生成するために必要な親フォルダも生成
+	// 既に存在する場合もtrue
+	bool bResult = QDir().mkpath( path );
+
+	QCOMPARE(bResult, bRetVal);
+}/*}}}*/
+/// QDir::mkpath() のテストデータセット
+void TestQDir::testmkpath_data()/*{{{*/
+{
+	// テストデータのタイトル設定
+	QTest::addColumn<QString>("path");
+	QTest::addColumn<bool>("bRetVal");
+	
+	// テストセット登録
+	QString strPath;
+	// 有効なパス
+	strPath = QDir::currentPath();
+	strPath.append("/test/testes/testdir");
+	QTest::newRow("case default") << strPath << true;
+	// 相対的なパス
+	strPath = "test/testes/testdir";
+	QTest::newRow("case relative dirPath") << strPath << true;
+	// 無効なパス
+	strPath = "?????????????????";
+	QTest::newRow("case invalid dirPath") << strPath << false;
+}/*}}}*/
+
+// テスト部
+void TestQDir::testcleanabsolutefilepath()/*{{{*/
+{
+	// タイトルからテストデータの取得
+	QFETCH(QString, directory);
+	QFETCH(QString, relative);
+	QFETCH(QString, absolute);
+	// 式を評価
+	QDir dir(directory);
+	// 絶対パス情報に変換
+	QString path = dir.absoluteFilePath( relative );
+//	QCOMPARE(path, absolute); // システムパス(.または..)が残るため、失敗するケースが有る
+	path = QDir::cleanPath( path );
+	QCOMPARE(path, absolute);
+
+}/*}}}*/
+// データセット部
+void TestQDir::testcleanabsolutefilepath_data()/*{{{*/
+{
+	// テストデータのタイトル設定
+	QTest::addColumn<QString>("directory");
+	QTest::addColumn<QString>("relative");
+	QTest::addColumn<QString>("absolute");
+	
+	// テストセット登録
+	QTest::newRow("current Path") << "C:/Data" << "ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("current sysPath") << "C:/Data" << "./ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("twice current sysPath") << "C:/Data" << "././ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("parent sysPath") << "C:/Qt" << "../Data/ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("twice parent sysPath") << "C:/Qt/bin" << "../../Data/ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("twice parent sysPath") << "C:/Qt/bin4" << "../../Data/ie6.png" << "C:/Data/ie6.png";
+}/*}}}*/
+
+// テスト部
+void TestQDir::testpath()/*{{{*/
+{
+	// タイトルからテストデータの取得
+	QFETCH(QString, path);
+	QFETCH(QString, dirPath);
+	// 式を評価
+	QDir dir(path);
+	path = dir.path();
+	QCOMPARE(path, dirPath);
+
+}/*}}}*/
+// データセット部
+void TestQDir::testpath_data()/*{{{*/
+{
+	// テストデータのタイトル設定
+	QTest::addColumn<QString>("path");
+	QTest::addColumn<QString>("dirPath");
+	
+	// テストセット登録
+	QTest::newRow("case dir path") << "C:/Data" << "C:/Data";
+	QTest::newRow("case dir path with sep") << "C:/Data/" << "C:/Data";
+	//QTest::newRow("case file path") << "C:/Data/ie6.png" << "C:/Data"; //FAIL!
+}/*}}}*/
+
+QTEST_MAIN(TestQDir)
+#include "testqdir.moc"
