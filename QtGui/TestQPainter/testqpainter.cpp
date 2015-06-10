6054d623e00478df0a3e7d5999e9323a662eb8b5
dNaga392 <dNagayama392@gmail.com>
2015-06-11 01:42:20 +0900

    add test for QPainter::drawText with rotation

--- QtGui/TestQPainter/testqpainter.cpp
+++ QtGui/TestQPainter/testqpainter.cpp
@@ -1,19 +1,29 @@
-// QPainterのテスト
-// testqpainter.cpp
+///==========================================
+///	@brief	QPainterのテスト
+///	@file	testqpainter.cpp
+///==========================================
 
 #include <QtTest/QtTest>
-#include <QPainter>
 
+#include <QtGui/QPainter>
 
+#define RGB_WHITE	QColor( 0xFF, 0xFF, 0xFF ).rgb()
+#define RGB_BLACK	QColor( 0x00, 0x00, 0x00 ).rgb()
+#define RGB_RED		QColor( 0xFF, 0x00, 0x00 ).rgb()
+#define RGB_GREEN	QColor( 0x00, 0xFF, 0x00 ).rgb()
+#define RGB_BLUE	QColor( 0x00, 0x00, 0xFF ).rgb()
 
-#define COLOR_WHITE	QColor(0xFF, 0xFF, 0xFF).rgb()
-#define COLOR_RED	QColor(0xFF, 0x00, 0x00).rgb()
-#define COLOR_GREEN	QColor(0x00, 0xFF, 0x00).rgb()
-#define COLOR_BLUE	QColor(0x00, 0x00, 0xFF).rgb()
+#ifdef M_PI
+#define PI			M_PI
+#else
+#define PI			3.14159265358979323846
+#endif // M_PI
 
-class TestQPainter: public QObject/*{{{*/
+class TestQPainter/*{{{*/
+	: public QObject
 {
 	Q_OBJECT
+
 private slots:
 // bool QPainter::drawLine () const
 	void drawLine();
@@ -30,7 +40,117 @@ private slots:
 	void saveAndRestore2_data();
 	void saveAndRestoreWinStyle();
 	void saveAndRestoreWinStyle_data();
+	///	Rotation Test
+	void QPainter_drawText_withRotation();
+
+private:
+	QPoint	rotational_conversion( const QPoint & point, int angle );
+
 };/*}}}*/
+
+QPoint	TestQPainter::rotational_conversion( const QPoint & point, int angle )/*{{{*/
+{
+	qDebug() << "angle =" << angle;
+	angle %= 360;
+	if ( angle < 0 )
+	{
+		angle += 360;
+	}
+	qDebug() << "angle =" << angle;
+
+	int x;
+	int y;
+	if ( angle == 0 )
+	{
+		x = point.x() * ( 1 ) - point.y() * ( 0 );
+		y = point.x() * ( 0 ) + point.y() * ( 1 );
+	}
+	else if ( angle == 90 )
+	{
+		x = point.x() * ( 0 ) - point.y() * ( 1 );
+		y = point.x() * ( 1 ) + point.y() * ( 0 );
+	}
+	else if ( angle == 180 )
+	{
+		x = point.x() * ( -1 ) - point.y() * (  0 );
+		y = point.x() * (  0 ) + point.y() * ( -1 );
+	}
+	else if ( angle == 270 )
+	{
+		x = point.x() * (  0 ) - point.y() * ( -1 );
+		y = point.x() * ( -1 ) + point.y() * (  0 );
+	}
+	else
+	{
+		double radian = angle * PI / 180;
+		x = (int)( (double)point.x() * cos( radian ) - (double)point.y() * sin( radian ) );
+		y = (int)( (double)point.x() * sin( radian ) + (double)point.y() * cos( radian ) );
+	}
+
+	QPoint rotated( x, y );
+	return rotated;
+}/*}}}*/
+
+void	TestQPainter::QPainter_drawText_withRotation()/*{{{*/
+{
+	QSize pix_size( 300, 200 );
+	QPixmap pix( pix_size );
+//	pix.fill( QColor( RGB_GREEN ) );
+
+	QPainter painter( &pix );
+
+	// 背景
+	painter.setBackground( QColor( RGB_WHITE ) );
+	QPaintDevice * device = NULL;
+	device = painter.device();
+	Q_CHECK_PTR( device );
+	painter.eraseRect( 0, 0, device->width(), device->height() );
+
+	// 描画準備
+	painter.setPen( QColor( RGB_BLACK ) );
+	QString text("test");
+	int angle = 90; // 度
+	// 描画点
+	QPoint draw_point( 50, 100 );
+	QPoint center_point( device->width() / 2, device->height() / 2 );
+	// 描画点補助線描画
+	painter.save();
+	painter.setPen( QColor( RGB_RED ) );
+	painter.drawLine( 0, draw_point.y(), device->width(), draw_point.y() );
+	painter.drawLine( draw_point.x(), 0, draw_point.x(), device->height() );
+	painter.restore();
+	// 中心テキスト描画
+	painter.drawText( center_point, "center" );
+	// 中心点補助線描画
+	painter.save();
+	painter.setPen( QColor( RGB_GREEN ) );
+	painter.drawLine( 0, center_point.y(), device->width(), center_point.y() );
+	painter.drawLine( center_point.x(), 0, center_point.x(), device->height() );
+	painter.restore();
+
+	painter.rotate( angle * ( -1 ) );
+
+
+	// 変換先の描画点を計算
+	QPoint shift_point( draw_point.x() - center_point.x(), draw_point.y() - center_point.y() );
+	QPoint rotated_shift( rotational_conversion( shift_point, angle ) );
+
+	// 元のデバイスサイズのため意味なし
+	//device = painter.device();
+	//Q_CHECK_PTR( device );
+	//QPoint rotated_center( device->width() / 2, device->height() / 2 );
+	QPoint rotated_center(rotational_conversion( center_point, angle ) );
+	QPoint rotated_point( rotated_shift.x() + rotated_center.x(), rotated_shift.y() + rotated_center.y() );
+
+	painter.drawText( rotated_point, text );
+	qDebug() << "rotated_shift : " << rotated_shift;
+	qDebug() << "rotated_center : " << rotated_center;
+	qDebug() << "rotated_point : " << rotated_point;
+
+	painter.rotate( angle * ( -1 ) );
+
+	pix.save( "QPainter_drawText_withRotation.bmp" );
+}/*}}}*/
 
 // テスト部
 void TestQPainter::drawLine()/*{{{*/
@@ -45,7 +165,7 @@ void TestQPainter::drawLine()/*{{{*/
 	QSize size(500, 500);//width, height
 	QPixmap pix( size );
 	QPainter p( &pix );
-	p.setPen( QColor( COLOR_RED ) );
+	p.setPen( QColor( RGB_RED ) );
 	p.drawLine( x1, y1, x2, y2 );
 	pix.save( filename );
 	// 式を評価
@@ -80,7 +200,7 @@ void TestQPainter::drawRect()/*{{{*/
 	QSize size(500, 500);//width, height
 	QPixmap pix( size );
 	QPainter p( &pix );
-	p.setPen( QColor( COLOR_RED ) );
+	p.setPen( QColor( RGB_RED ) );
 
 	QRect rect;
 	if ( x1 > x2 ) 
@@ -139,7 +259,7 @@ void TestQPainter::drawText()/*{{{*/
 	QPixmap pix( size );
 
 	QPainter p( &pix );
-	QPen pen( QColor( COLOR_WHITE ) );
+	QPen pen( QColor( RGB_WHITE ) );
 	QFont font( family, pointSize );
 	p.setPen( pen );
 	p.setFont( font );
@@ -180,8 +300,8 @@ void TestQPainter::fillRect()/*{{{*/
 	QSize size(500, 500);//width, height
 	QPixmap pix( size );
 	QPainter p( &pix );
-	//p.setBrush( QBrush( QColor( COLOR_RED ) ) );
-	QBrush brush( QColor( COLOR_RED ) );
+	//p.setBrush( QBrush( QColor( RGB_RED ) ) );
+	QBrush brush( QColor( RGB_RED ) );
 	p.setBrush( brush );
 
 	QRect rect;
@@ -206,7 +326,7 @@ void TestQPainter::fillRect()/*{{{*/
 		rect.setBottom( y1 );
 	}
 	p.fillRect(rect, p.brush());
-	//p.fillRect(rect, QColor( COLOR_RED ));
+	//p.fillRect(rect, QColor( RGB_RED ));
 
 	pix.save( filename );
 	// 式を評価
@@ -241,10 +361,10 @@ void TestQPainter::saveAndRestore()/*{{{*/
 	QPixmap pix( size );
 	QPainter p( &pix );
 
-	QPen * newPen = new QPen( QColor( COLOR_WHITE ) );
+	QPen * newPen = new QPen( QColor( RGB_WHITE ) );
 	newPen->setWidth(1);
 
-	p.setPen( QColor( COLOR_RED ) );
+	p.setPen( QColor( RGB_RED ) );
 	p.drawLine( x1, y1, x2, y1 );
 
 	p.save();
@@ -292,9 +412,9 @@ void TestQPainter::saveAndRestore2()/*{{{*/
 	QPixmap pix( size );
 	QPainter p( &pix );
 
-	QPen rPen( QColor( COLOR_RED ) ); 
-	QPen gPen( QColor( COLOR_GREEN ) ); 
-	QPen bPen( QColor( COLOR_BLUE ) ); 
+	QPen rPen( QColor( RGB_RED ) ); 
+	QPen gPen( QColor( RGB_GREEN ) ); 
+	QPen bPen( QColor( RGB_BLUE ) ); 
 
 	rPen.setWidth(1);
 	gPen.setWidth(1);
@@ -355,10 +475,10 @@ void TestQPainter::saveAndRestoreWinStyle()/*{{{*/
 	QPixmap pix( size );
 	QPainter p( &pix );
 
-	QPen * newPen = new QPen( QColor( COLOR_WHITE ) );
+	QPen * newPen = new QPen( QColor( RGB_WHITE ) );
 	newPen->setWidth(1);
 
-	p.setPen( QColor( COLOR_RED ) );
+	p.setPen( QColor( RGB_RED ) );
 	p.drawLine( x1, y1, x2, y1 );
 
 	QPen oldPen = p.pen();
@@ -394,3 +514,4 @@ void TestQPainter::saveAndRestoreWinStyle_data()/*{{{*/
 
 QTEST_MAIN(TestQPainter)
 #include "testqpainter.moc"
+
