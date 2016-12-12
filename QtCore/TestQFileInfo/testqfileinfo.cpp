390a9450ea4f8f76d7d9df0bb1d0884d9d66d854
nagayama <nagayama@ipl.co.jp>
2016-12-12 16:42:41 +0900

    add 2 text cases of QFileInfo

--- QtCore/TestQFileInfo/testqfileinfo.cpp
+++ QtCore/TestQFileInfo/testqfileinfo.cpp
@@ -3,19 +3,59 @@
 	@brief	QFileInfoのテスト
 ******************************************************************* */
 #include <QtTest/QtTest>
-#include <QFileInfo>
+#include <QtCore/QFileInfo>
 
-class TestQFileInfo: public QObject
+class TestQFileInfo : public QObject
 {
 	Q_OBJECT
 
 private Q_SLOTS:
+	void exists_data();
+	void exists();
+	void isDir_data();
+	void isDir();
 	void path();
 	void test();
 	void test_data();
 	void dirtest();
 	void dirtest_data();
 };
+
+void TestQFileInfo::exists_data()
+{
+	QTest::addColumn<QString>("path");
+	QTest::addColumn<bool>("expected");
+
+	QTest::newRow("Dir") << QCoreApplication::instance()->applicationDirPath() << true;
+	QTest::newRow("File") << QCoreApplication::instance()->applicationFilePath() << true;
+	QTest::newRow("NoExistsDir") << QCoreApplication::instance()->applicationDirPath().append("/NoExistsDir") << false;
+	QTest::newRow("NoExistsFile") << QCoreApplication::instance()->applicationDirPath().append("/NoExistsFile.dat") << false;
+}
+void TestQFileInfo::exists()
+{
+	QFETCH(QString, path);
+	QFETCH(bool, expected);
+
+	QCOMPARE(QFileInfo(path).exists(), expected);
+}
+
+void TestQFileInfo::isDir_data()
+{
+	QTest::addColumn<QString>("path");
+	QTest::addColumn<bool>("expected");
+
+	QTest::newRow("Dir") << QCoreApplication::instance()->applicationDirPath() << true;
+	QTest::newRow("File") << QCoreApplication::instance()->applicationFilePath() << false;
+	QTest::newRow("NoExistsDir") << QCoreApplication::instance()->applicationDirPath().append("/NoExistsDir") << false;
+	QTest::newRow("NoExistsFile") << QCoreApplication::instance()->applicationDirPath().append("/NoExistsFile.dat") << false;
+}
+void TestQFileInfo::isDir()
+{
+	QFETCH(QString, path);
+	QFETCH(bool, expected);
+
+	QCOMPARE(QFileInfo(path).isDir(), expected);
+}
 
 void TestQFileInfo::path()
 {
@@ -30,6 +70,22 @@ void TestQFileInfo::path()
 	QCOMPARE( fi.path(), path ); // TRUE
 }
 
+// データセット部
+void TestQFileInfo::test_data()/*{{{*/
+{
+	// テストデータのタイトル設定
+	QTest::addColumn<QString>("directory");
+	QTest::addColumn<QString>("relative");
+	QTest::addColumn<QString>("absolute");
+	
+	// テストセット登録
+	QTest::newRow("current Path") << "C:/Data" << "ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("current sysPath") << "C:/Data" << "./ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("twice current sysPath") << "C:/Data" << "././ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("parent sysPath") << "C:/Qt" << "../Data/ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("twice parent sysPath") << "C:/Qt/bin" << "../../Data/ie6.png" << "C:/Data/ie6.png";
+	QTest::newRow("twice parent sysPath") << "C:/Qt/bin4" << "../../Data/ie6.png" << "C:/Data/ie6.png";
+}/*}}}*/
 // テスト部
 void TestQFileInfo::test()/*{{{*/
 {
@@ -67,34 +123,7 @@ void TestQFileInfo::test()/*{{{*/
 	QCOMPARE(path, absolute); // TRUE
 
 }/*}}}*/
-// データセット部
-void TestQFileInfo::test_data()/*{{{*/
-{
-	// テストデータのタイトル設定
-	QTest::addColumn<QString>("directory");
-	QTest::addColumn<QString>("relative");
-	QTest::addColumn<QString>("absolute");
-	
-	// テストセット登録
-	QTest::newRow("current Path") << "C:/Data" << "ie6.png" << "C:/Data/ie6.png";
-	QTest::newRow("current sysPath") << "C:/Data" << "./ie6.png" << "C:/Data/ie6.png";
-	QTest::newRow("twice current sysPath") << "C:/Data" << "././ie6.png" << "C:/Data/ie6.png";
-	QTest::newRow("parent sysPath") << "C:/Qt" << "../Data/ie6.png" << "C:/Data/ie6.png";
-	QTest::newRow("twice parent sysPath") << "C:/Qt/bin" << "../../Data/ie6.png" << "C:/Data/ie6.png";
-	QTest::newRow("twice parent sysPath") << "C:/Qt/bin4" << "../../Data/ie6.png" << "C:/Data/ie6.png";
-}/*}}}*/
 
-// テスト部
-void TestQFileInfo::dirtest()/*{{{*/
-{
-	// タイトルからテストデータの取得
-	QFETCH(QString, path);
-	QFETCH(QString, dirPath);
-	// 式を評価
-	QDir dir(path);
-	QCOMPARE(dir.path(), dirPath);
-
-}/*}}}*/
 // データセット部
 void TestQFileInfo::dirtest_data()/*{{{*/
 {
@@ -106,6 +135,17 @@ void TestQFileInfo::dirtest_data()/*{{{*/
 	QTest::newRow("dir path") << "C:/Data" << "C:/Data";
 	QTest::newRow("dir path with sep") << "C:/Data/" << "C:/Data";
 	//QTest::newRow("file path") << "C:/Data/ie6.png" << "C:/Data"; //FAIL!
+}/*}}}*/
+// テスト部
+void TestQFileInfo::dirtest()/*{{{*/
+{
+	// タイトルからテストデータの取得
+	QFETCH(QString, path);
+	QFETCH(QString, dirPath);
+	// 式を評価
+	QDir dir(path);
+	QCOMPARE(dir.path(), dirPath);
+
 }/*}}}*/
 
 QTEST_MAIN(TestQFileInfo)
