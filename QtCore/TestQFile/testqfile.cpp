cf0790d79d7a592eb6e3cdf0123a59a24a279cea
nagayama <nagayama@ipl.co.jp>
2016-12-27 17:10:17 +0900

    add test case of QFile

--- QtCore/TestQFile/testqfile.cpp
+++ QtCore/TestQFile/testqfile.cpp
@@ -5,70 +5,68 @@
 #include <QtCore/QFile>
 #include <QtCore/QStringList>
 
-#include <math.h>
+#include <cmath>
+
+#ifdef QT_DEBUG
+//#define DEBUG_OUTPUT
+#endif // QT_DEBUG
 
 ///	QFileのテストクラス
-class TestQFile/*{{{*/
+class TestQFile
 	: public QObject
 {
 	Q_OBJECT
-private slots:
-	void testseek_tobegin();
+
+private Q_SLOTS:
+	void readLine_data();
+	void readLine();
 	void testseek_tobegin_data();
+	void testseek_tobegin();
 
-#ifdef QT_DEBUG
-	/// [DEBUG]QStringListのダンプ
-	bool dumpQStringList( QStringList & list, QString & fileName );
-#endif // QT_DEBUG
+};
 
-};/*}}}*/
-
-/// The test of QFile::seek() for seak to beginning
-void TestQFile::testseek_tobegin()/*{{{*/
+void TestQFile::readLine_data()
 {
-	// タイトルからテストデータの取得
+	QTest::addColumn<QString>("strFilePath");
+
+	// テストセット登録
+	QString strPath;
+	// 有効なパス
+	strPath = QCoreApplication::applicationDirPath();
+	strPath.append("/../file/sample_file.txt");
+	QTest::newRow("case default") << strPath;
+}
+
+void TestQFile::readLine()
+{
 	QFETCH(QString, strFilePath);
 
-	QFile file( strFilePath );
-
-	bool bResult;
-	bResult = file.open( QIODevice::ReadOnly | QIODevice::Text );
-	if ( !bResult )
+	QFile file(strFilePath);
+	if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
 	{
-		qFatal( ("Error: file is not opened.[ %s ]"), qPrintable( strFilePath ) );
+		qFatal(("Error: file is not opened.[%s]"), qPrintable(strFilePath));
 	}
-
-	QStringList list1;
-	while ( !file.atEnd() )
+	while (!file.atEnd())
 	{
-		QByteArray baLine = file.readLine();
-		//qDebug( ("Read: [ %s ]"), baLine.constData() );
-		qDebug( ("Read: [ %f ]"), (double)atof( baLine.constData() ) );
-		list1 << QString( baLine );
+		QString str(file.readLine());
+		if (!file.atEnd())
+		{
+			// a string read by qtextstream contains newline charactor(s).
+			bool actual = str.contains(QRegExp("\r$|\n$|\r\n$"));
+#ifdef DEBUG_OUTPUT
+			if (!actual)
+			{
+				qDebug() << "not contain newline charactor(s) :" << str;
+			}
+#endif // DEBUG_OUTPUT
+			QCOMPARE(actual, true);
+		}
 	}
-
-	qDebug( ("-------- Seek to beginning --------") );
-	file.seek( 0 ); // seak to beginning
-	
-	QStringList list2;
-	while ( !file.atEnd() )
-	{
-		QByteArray baLine = file.readLine();
-		//qDebug( ("Read: [ %s ]"), baLine.constData() );
-		qDebug( ("Read: [ %f ]"), (double)atof( baLine.constData() ) );
-		list2 << QString( baLine );
-	}
-
-	QCOMPARE( list1.size(), list2.size() ); // equal size
-	for ( int i = 0; i < list1.size(); ++i  )
-	{
-		QCOMPARE( list1[ i ], list2[ i ] ); // equal string
-	}
-
 	file.close();
-}/*}}}*/
+}
+
 /// The test data of QFile::seek() for seak to beginning
-void TestQFile::testseek_tobegin_data()/*{{{*/
+void TestQFile::testseek_tobegin_data()
 {
 	// テストデータのタイトル設定
 	QTest::addColumn<QString>("strFilePath");
@@ -76,36 +74,55 @@ void TestQFile::testseek_tobegin_data()/*{{{*/
 	// テストセット登録
 	QString strPath;
 	// 有効なパス
-	strPath = QDir::currentPath();
-	strPath.append("/data/sample_file.txt");
+	strPath = QCoreApplication::applicationDirPath();
+	strPath.append("/../file/sample_file.txt");
 	QTest::newRow("case default") << strPath;
-}/*}}}*/
+}
 
-#ifdef QT_DEBUG
-
-#include <QtCore/QFile>
-#include <QtCore/QTextStream>
-
-/// [DEBUG]QStringListのダンプ
-bool TestQFile::dumpQStringList( QStringList & list, QString & fileName )/*{{{*/
+/// The test of QFile::seek() for seak to beginning
+void TestQFile::testseek_tobegin()
 {
-	QFile file(fileName);
-	bool bRet = file.open(QIODevice::WriteOnly);
-	if (!bRet) {
-		// ERROR
-		qDebug("%s, %d: [Error] file is not opened.", __FILE__, __LINE__);
-		return false;
-	}
-	QTextStream out(&file);
-	foreach(QString text, list)
-	{
-		out << text << endl;
-	}
-	file.close();
-	return true;
-}/*}}}*/
+	// タイトルからテストデータの取得
+	QFETCH(QString, strFilePath);
 
-#endif // QT_DEBUG
+	QFile file(strFilePath);
+
+	bool bResult;
+	bResult = file.open(QIODevice::ReadOnly | QIODevice::Text);
+	if (!bResult)
+	{
+		qFatal(("Error: file is not opened.[%s]"), qPrintable(strFilePath));
+	}
+
+	QStringList list1;
+	while (!file.atEnd())
+	{
+		QByteArray baLine = file.readLine();
+		//qDebug() << "Read: [" << baLine.constData() << "]";
+		//qDebug() << "Read: [" << (double)atof(baLine.constData()) << "]";
+		list1 << QString(baLine);
+	}
+
+	//qDebug(("-------- Seek to beginning --------"));
+	file.seek(0); // seak to beginning
+	
+	QStringList list2;
+	while (!file.atEnd())
+	{
+		QByteArray baLine = file.readLine();
+		//qDebug(("Read: [%s]"), baLine.constData());
+		//qDebug(("Read: [%f]"), (double)atof(baLine.constData()));
+		list2 << QString(baLine);
+	}
+
+	QCOMPARE(list1.size(), list2.size()); // equal size
+	for (int i = 0; i < list1.size(); ++i)
+	{
+		QCOMPARE(list1[i], list2[i]); // equal string
+	}
+
+	file.close();
+}
 
 QTEST_MAIN(TestQFile)
 #include "testqfile.moc"
