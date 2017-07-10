2075bf0977af082a3f9e3bb3d547ea238882d627
nagayama <nagayama@ipl.co.jp>
2017-07-10 15:39:58 +0900

    :green_heart: Update a test file for QtGlobal

--- QtGlobal/TestQtGlobal/testqtglobal.cpp
+++ QtGlobal/TestQtGlobal/testqtglobal.cpp
@@ -10,6 +10,7 @@ private slots:
 // const T & qMax ( const T & value1, const T & value2 )
 	void qMaxInt();
 	void qMaxInt_data();
+	//void test_foreach();
 };
 
 // const T & qMax ( const T & value1, const T & value2 )
@@ -38,6 +39,27 @@ void TestQtGlobal::qMaxInt_data()/*{{{*/
 	QTest::newRow("qMax(8,-6)") << 8 << -6 << 8;
 	QTest::newRow("qMax(-8,-6)") << -8 << -6 << -6;
 }/*}}}*/
+
+//void TestQtGlobal::test_foreach()
+//{
+//	QList<QString> intList;
+//	intList.append(QString("Bob"));
+//	intList.append(QString("Bob2"));
+//	intList.append(QString("Bob3"));
+//	intList.append(QString("Bob4"));
+//	int index=0;
+//	foreach(QString item, intList)
+//	{
+//		if(index%3==0)
+//		{
+//			qDebug() << "Bob is" << index << "\n";
+//			++index;
+//			continue;
+//		}
+//		qDebug() << "Bob is" << item << "\n";
+//			++index;
+//	}
+//}
 
 QTEST_MAIN(TestQtGlobal)
 #include "testqtglobal.moc"
